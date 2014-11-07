d4cd917f2c86cbd7043fee0a6ae343da66c45e01
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-11-08 00:06:19 +0530

    Update dice_simulator.py

--- python/dice_simulator.py
+++ python/dice_simulator.py
@@ -7,8 +7,8 @@
 */
 
 import random
-def dice_stimulator(number_of_rolls,dice_size):
+def dice_simulator(number_of_rolls,dice_size):
   for i in range(0,number_of_rolls):
     output = random.randint(1,dice_size)
     print "Die %d is rolled and the output is %d" %(i+1,output)
-dice_stimulator(2,6)
+dice_simulator(2,6)
