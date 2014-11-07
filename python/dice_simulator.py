0dfaa45c79a90afbd3c35e9c5ec7477190855446
Turlapaty Kaushik <turlapatykaushik95@gmail.com>
2014-11-07 18:33:33 +0530

    New project added

---
+++ python/dice_simulator.py
@@ -0,0 +1,15 @@
+/*
+ * @turlapatykaushik
+ * github url : github.com/turlapatykaushik
+
+ * project description : We all know dice game and this project aims at finding
+   the outputs of a dice
+   
+*/
+
+import random
+def dice_stimulator(number_of_rolls,dice_size):
+  for i in range(0,number_of_rolls):
+    output = random.randint(1,dice_size)
+    print "Die %d is rolled and the output is %d" %(i+1,output)
+dice_stimulator(2,6)
